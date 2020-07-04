1abd72ca4ebd971758891df11093d650df93ace1
yungtouch-sudo <yungtouch@yandex.ru>
2020-07-04 17:44:18 +0300

    html+css

--- js/slider.js
+++ js/slider.js
@@ -4,13 +4,12 @@ var slideInterval = setInterval(nextSlide, 5000);
 var header = document.querySelector('.header');
 var mainAvantages = document.querySelector('.main-advantages');
 var mainPreview = document.querySelector('.main_preview');
-
 var shopInfo = document.querySelector('.shop-info');
 var mainNews = document.querySelector('.main-news');
 var footer = document.querySelector('.footer');
-
 var colors = ['#849d8f', '#8996a6', '#9d8b84'];
 var currentColor = 0;
+var a = [header, mainAvantages, mainPreview, shopInfo, mainNews, footer];
 
 function nextSlide() {
   slides[currentSlide].className = 'slide';
@@ -21,10 +20,8 @@ function nextSlide() {
     currentColor = 0;
   }
 
-  header.style.background = colors[currentColor];
-  mainAvantages.style.background = colors[currentColor];
-  mainPreview.style.background = colors[currentColor];
-  shopInfo.style.background = colors[currentColor];
-  mainNews.style.background = colors[currentColor];
-  footer.style.background = colors[currentColor];
+  for (var i = 0; i < a.length; i += 1){
+    a[i].style.background = colors[currentColor];
+  }
+
 };
